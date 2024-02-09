2d608d7fbf40d11c9b4836673f1a714dbcf96d3d
Hadi A <hammar24@students.polytechnic.org>
2024-02-08 21:57:44 -0800

    trasacrion class

--- Transaction.java
+++ Transaction.java
@@ -1,1 +1,31 @@
-test 
+//this object holds the infortmation stored in the transaction obects, will be used in the higher degree classes(ones that use this object).
+public class Transaction{ 
+
+    //fields
+private double charge;
+private int day;
+private int month;
+
+
+//constructors
+
+public Transaction(double myCharge, int myDay, int myMonth){
+charge = myCharge;
+day = myDay;
+month = myMonth;
+}
+
+//methods
+public double getCharge(){
+    return charge;
+}
+
+public int getDay(){
+    return day;
+}
+
+public int getMonth(){
+    return month;
+}
+
+}
